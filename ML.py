1166e0855453ec758ca19339ed91980a6169625d
liammeyer <76468475+liammeyer@users.noreply.github.com>
2024-06-06 11:12:56 -0500

    Update ML.py
    Got it working - 50% accuracy roughly. Goal of 80%+

--- ML.py
+++ ML.py
@@ -1,26 +1,43 @@
-
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
 
-#Load Data
-attack = pd.read_csv('Attack.csv')
-patient = pd.read_csv('patientMonitoring.csv')
+# Load Data
+attack = pd.read_csv('Attack.csv', low_memory=False)
+patient = pd.read_csv('patientMonitoring.csv', low_memory=False)
 environment = pd.read_csv('environmentMonitoring.csv')
 
-attack.replace([np.inf, -np.inf], np.nan, inplace=True) #replace all pos inf and neg inf with nan (not any number)
-patient.replace([np.inf, -np.inf], np.nan, inplace=True) #replace all pos inf and neg inf with nan (not any number)
-environment.replace([np.inf, -np.inf], np.nan, inplace=True) #replace all pos inf and neg inf with nan (not any number)
+# Preprocess and handle missing data if needed
+# attack.replace([np.inf, -np.inf], np.nan, inplace=True)
+# patient.replace([np.inf, -np.inf], np.nan, inplace=True)
+# environment.replace([np.inf, -np.inf], np.nan, inplace=True)
+# attack.dropna(inplace=True)
+# patient.dropna(inplace=True)
+# environment.dropna(inplace=True)
 
-attack = attack.dropna() #REMOVES ALL ROWS containing 'not any numberic' values - probably N/A but just in case
-patient = patient.dropna() #REMOVES ALL ROWS containing 'not any numberic' values - probably N/A but just in case
-environment = environment.dropna() #REMOVES ALL ROWS containing 'not any numberic' values - probably N/A but just in case
+# Combine patient and attack data
+combinedPatientAttack = pd.concat([patient, attack], axis=0)
 
+# Prepare Data
+X = combinedPatientAttack.drop('label', axis=1)  # Drop the target column to create a feature set
+X = X.select_dtypes(exclude='object')  # This excludes all columns of type 'object', typically strings
 
-#Prepare Data
-X = data.drop('label', axis=1)  # Assuming 'target_column_name' is the name of your target column
-y = data['label']
+y = combinedPatientAttack['label']  # Keep only the target column
 
-# Step 3: Train/Test Split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Train/Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# Initialize MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(2, 2), activation='relu', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
+
+# Re-train using the training data
+mlp.fit(X_train, y_train)  # Use X_train and y_train here
+
+# Predict the labels for the test set
+predictions = mlp.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, predictions)
+print("Model Accuracy:", accuracy)
