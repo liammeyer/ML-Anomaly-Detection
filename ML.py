f0c895874c3192caab20c6324145d3b05587e777
Evan Myran <evankmyran@gmail.com>
2024-06-07 15:57:50 -0500

    ML.py

--- ML.py
+++ ML.py
@@ -41,7 +41,7 @@ y = temps['label']  # Keep only the target column
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # Initialize MLPClassifier
-
+# For Healthcare Dataset
 '''
 #2 layers - 2 neurons in each - 51% accuracy
 mlp = MLPClassifier(hidden_layer_sizes=(2, 2), activation='relu', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
@@ -58,10 +58,16 @@ mlp = MLPClassifier(hidden_layer_sizes=(8, 4, 2), activation='tanh', solver='ada
 #3 layers - 8,4,2 neurons in each - logistic activation function, performs similar to tanh - 95% accuracy
 mlp = MLPClassifier(hidden_layer_sizes=(8, 4, 2), activation='logistic', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
 '''
-
+# For Healthcare Dataset
 # Use a tanh activation function because its useful when there is data centered around zero which is true in our case
 #3 layers - 7,4,2 neurons in each - 97.8% accuracy
-mlp = MLPClassifier(hidden_layer_sizes=(7, 4, 2), activation='tanh', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
+# mlp = MLPClassifier(hidden_layer_sizes=(7, 4, 2), activation='tanh', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
+
+# For temp dataset
+mlp = MLPClassifier(hidden_layer_sizes=(7, 4, 2), activation='relu', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
+
+
+
 
 # Implementing Grid Search for hyperparameter tuning
 
