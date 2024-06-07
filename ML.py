566bf489930a764f37b1253ef41f15ae85d52398
Evan Myran <evankmyran@gmail.com>
2024-06-07 16:04:35 -0500

    ML.py

--- ML.py
+++ ML.py
@@ -64,9 +64,10 @@ mlp = MLPClassifier(hidden_layer_sizes=(8, 4, 2), activation='logistic', solver=
 # mlp = MLPClassifier(hidden_layer_sizes=(7, 4, 2), activation='tanh', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
 
 # For temp dataset
-#2 layers - 2,2 neurons in each - 78.4% accuracy
-#2 layers - 4,2 neurons in each - 78.3% accuracy
-mlp = MLPClassifier(hidden_layer_sizes=(16, 8, 2), activation='tanh', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
+#2 layers tanh- 2,2 neurons in each - 78.4% accuracy
+#2 layers tanh- 4,2 neurons in each - 78.3% accuracy
+#3 layers tanh- 16,8,2 neurons in each - 21.2% accuracy
+mlp = MLPClassifier(hidden_layer_sizes=(16, 8, 2), activation='relu', solver='adam', random_state=1, verbose=True, early_stopping=True, max_iter=300)
 
 
 
