16cbcbf82ed5c51d6a31e67ebfaa3f4ea7f295d4
Evan Myran <evankmyran@gmail.com>
2024-06-10 11:54:58 -0500

    ML.py

--- ML.py
+++ ML.py
@@ -40,7 +40,7 @@ y = temps['label']  # Keep only the target column
 
 
 # Train/Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
 
 # Initialize MLPClassifier
 # For Healthcare Dataset
