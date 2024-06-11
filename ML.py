a6d05a79c3ef995685564d7b8c7cba829e9b61b3
Evan Myran <evankmyran@gmail.com>
2024-06-11 14:15:25 -0500

    ML.py

--- ML.py
+++ ML.py
@@ -54,15 +54,15 @@ X_reshaped_train = X_scaled_train.reshape((X_scaled_train.shape[0], 1, X_scaled_
 
 # Define the LSTM model
 model = Sequential()
-model.add(LSTM(units=100, return_sequences=True, input_shape=(X_reshaped_train.shape[1], X_reshaped_train.shape[2])))
+model.add(LSTM(units=75, return_sequences=True, input_shape=(X_reshaped_train.shape[1], X_reshaped_train.shape[2])))
 model.add(Dropout(0.3))
-model.add(LSTM(units=50, return_sequences=False))
+model.add(LSTM(units=35, return_sequences=False))
 model.add(Dropout(0.3))
 model.add(Dense(units=1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Fit the model
-history = model.fit(X_reshaped_train, y_train, epochs=100, batch_size=32, validation_split=0.2, verbose=2)
+history = model.fit(X_reshaped_train, y_train, epochs=500, batch_size=32, validation_split=0.2, verbose=2)
 
 # Evaluate the model
 loss, accuracy = model.evaluate(X_reshaped_test, y_test)
